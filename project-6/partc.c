6dfcf408300491cf1ae7209e12b24a9cef12ae3a
Tristan <tristan@tristan-VirtualBox.(none)>
2012-11-24 13:03:21 -0700

    commiting

---
+++ project-6/partc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define NUM_THREADS 4
+int count = 0;
+
+void *incrementCounter( void *m)
+{
+	int i;
+	for (i = 0; i < 10; ++i)
+	{
+		int tempValue = count;
+		tempValue = tempValue + 1;
+		count = tempValue;
+	}
+	return NULL;
+}
+int main(int argc, char *argv[])
+{
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	long t;
+	for(t = 0; t < NUM_THREADS; t++)
+	{
+	rc = pthread_create(&threads[t], NULL, incrementCounter, (void *)t);
+	}
+	for(t = 0; t < NUM_THREADS; t++)
+	{
+		(void) pthread_join(threads[t], NULL);
+	}
+	printf("count %d\n",count);
+    return 0;
+}
